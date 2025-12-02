b6e4ae1ba57367acfb176a52e9afd67085ef0e16
jonsetzky <40266374+jonsetzky@users.noreply.github.com>
2025-12-02 12:35:52 +0200

    complete 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -23,31 +23,35 @@ const Content = ({ parts }) => {
 };
 
 const Total = ({ total }) => {
-  return <p>Number of exercises {total}</p>;
+  return (
+    <p>
+      Number of exercises {total.reduce((sum, part) => sum + part.exercises, 0)}
+    </p>
+  );
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
-
-  const parts = [part1, part2, part3];
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
       <Content parts={parts} />
-      <Total total={parts.reduce((sum, part) => sum + part.exercises, 0)} />
+      <Total total={parts} />
     </div>
   );
 };
