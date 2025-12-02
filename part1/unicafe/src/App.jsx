550fee6bf6d03dcde7de52ab8bf4a5623121fc17
jonsetzky <40266374+jonsetzky@users.noreply.github.com>
2025-12-02 12:51:50 +0200

    complete 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = (good / total) * 100;
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -18,6 +22,12 @@ const App = () => {
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <span> {bad}</span>
       <br />
+      <h1>statistics</h1>
+      <p>
+        total {total} <br />
+        average {isNaN(average) ? 0 : average.toFixed(2)} <br />
+        positive {isNaN(positive) ? 0 : positive.toFixed(2)} %
+      </p>
     </div>
   );
 };
