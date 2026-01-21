350db323418b4d2291943b756136a70651d357f0
Joona Granlund <40266374+jonsetzky@users.noreply.github.com>
2026-01-21 16:35:53 +0200

    complete 4.5

--- part4/src/utils/list_helper.js
+++ part4/src/utils/list_helper.js
@@ -6,7 +6,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce(
+    (fav, blog) => (fav === null || blog.likes > fav.likes ? blog : fav),
+    null,
+  );
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
