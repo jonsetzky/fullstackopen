993d5e817276b137fbe65035dad29d488494e0ed
Joona Granlund <40266374+jonsetzky@users.noreply.github.com>
2026-01-21 11:21:48 +0200

    fix error part3

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -101,7 +101,7 @@ app.post("/api/persons", express.json(), (req, res) => {
   res.json(newPerson);
 });
 
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
