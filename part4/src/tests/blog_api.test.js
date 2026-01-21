b6f2f5f4de6c82d6f07eb4ac339719480ad0ff14
Joona Granlund <40266374+jonsetzky@users.noreply.github.com>
2026-01-21 17:37:50 +0200

    complete 4.10

--- part4/src/tests/blog_api.test.js
+++ part4/src/tests/blog_api.test.js
@@ -17,35 +17,40 @@ describe("blogs", () => {
       .expect("Content-Type", /application\/json/);
   });
   test("returns correct amount of blogs", async () => {
-    await api
-      .get("/api/blogs")
-      .timeout(5000)
-      .expect(200)
-      .then((response) => {
-        assert.strictEqual(response.body.length, 2);
-      });
+    const response = await api.get("/api/blogs").timeout(5000).expect(200);
+
+    assert.strictEqual(response.body.length, (await Blog.find({})).length);
   });
   test("have author field", async () => {
-    await api
-      .get("/api/blogs")
-      .timeout(5000)
-      .expect(200)
-      .then((response) => {
-        for (const blog of response.body) {
-          assert("author" in blog);
-        }
-      });
+    const response = await api.get("/api/blogs").timeout(5000).expect(200);
+
+    for (const blog of response.body) {
+      assert("author" in blog);
+    }
   });
   test("have id field", async () => {
-    await api
-      .get("/api/blogs")
-      .timeout(5000)
-      .expect(200)
-      .then((response) => {
-        for (const blog of response.body) {
-          assert("id" in blog);
-        }
-      });
+    const response = await api.get("/api/blogs").timeout(5000).expect(200);
+
+    for (const blog of response.body) {
+      assert("id" in blog);
+    }
+  });
+  test("can be added to the db", async () => {
+    const originalCount = (await Blog.find({})).length;
+
+    const blog = {
+      author: `Test Author ${originalCount + 1}`,
+      title: `Test Title ${originalCount + 1}`,
+      url: `http://testurl${originalCount + 1}.com`,
+      likes: originalCount + 1,
+    };
+
+    const newBlog = (
+      await api.post("/api/blogs").send(blog).timeout(5000).expect(201)
+    ).body;
+
+    assert.strictEqual((await Blog.find({})).length, originalCount + 1);
+    assert.deepStrictEqual(newBlog, { ...blog, id: newBlog.id });
   });
 });
 
