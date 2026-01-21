d3b16e44043259b4860c771b3272b285b02a3923
Joona Granlund <40266374+jonsetzky@users.noreply.github.com>
2026-01-21 11:29:11 +0200

    fix part2 env variables

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const baseUrl = process.env.VITE_BASE_URL || "http://localhost:3001/persons";
+const baseUrl =
+  import.meta.env.VITE_BASE_URL || "http://localhost:3001/persons";
 
 const getAll = () => {
   return axios.get(baseUrl).then((response) => response.data);
