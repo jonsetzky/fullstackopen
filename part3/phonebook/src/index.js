422495b12fe6ba0386b45dae8158d193b7bd4440
Joona Granlund <40266374+jonsetzky@users.noreply.github.com>
2026-01-21 15:03:14 +0200

    complete 3.22

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const express = require("express");
-var morgan = require("morgan");
-const mongoose = require("mongoose");
+const express = require('express')
+var morgan = require('morgan')
+const mongoose = require('mongoose')
 
 const {
   addPerson,
@@ -11,121 +11,121 @@ const {
   deletePerson,
   getPersonByName,
   updatePerson,
-} = require("./mongo.js");
+} = require('./mongo.js')
 
 // this fixes querySrv problem with mongoose
 // https://www.mongodb.com/community/forums/t/error-querysrv-econnrefused-mongodb/259042
-require("node:dns/promises").setServers(["1.1.1.1"]);
+require('node:dns/promises').setServers(['1.1.1.1'])
 
-const mongodb_url = process.env.MONGODB_URL;
-mongoose.set("strictQuery", false);
+const mongodb_url = process.env.MONGODB_URL
+mongoose.set('strictQuery', false)
 mongoose
   .connect(mongodb_url, { family: 4 })
-  .then((result) => {
-    console.log("connected to MongoDB");
+  .then(() => {
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error);
-  });
+    console.log('error connecting to MongoDB:', error)
+  })
 
-const app = express();
+const app = express()
 
-app.use(express.static("dist"));
-app.use(express.json());
+app.use(express.static('dist'))
+app.use(express.json())
 app.use(
   morgan(function (tokens, req, res) {
     return [
       tokens.method(req, res),
       tokens.url(req, res),
       tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
       JSON.stringify(req.body),
-    ].join(" ");
+    ].join(' ')
   }),
-);
+)
 
-app.get("/api/persons", async (req, res) => {
-  res.json(await getAllPersons());
-});
-app.get("/api/persons/:id", async (req, res, next) => {
+app.get('/api/persons', async (req, res) => {
+  res.json(await getAllPersons())
+})
+app.get('/api/persons/:id', async (req, res, next) => {
   await getPersonById(req.params.id)
     .then((persons) => {
-      const person = persons[0];
+      const person = persons[0]
       if (!person) {
-        return res.status(404).json({ error: "person not found" });
+        return res.status(404).json({ error: 'person not found' })
       }
-      res.json(person);
+      res.json(person)
     })
-    .catch(next);
-});
-app.delete("/api/persons/:id", async (req, res) => {
-  await deletePerson(req.params.id);
-  res.end();
-});
-app.post("/api/persons", express.json(), async (req, res) => {
+    .catch(next)
+})
+app.delete('/api/persons/:id', async (req, res) => {
+  await deletePerson(req.params.id)
+  res.end()
+})
+app.post('/api/persons', express.json(), async (req, res) => {
   const newPerson = {
     ...req.body,
-  };
+  }
 
   if (!newPerson.name || !newPerson.number) {
-    return res.status(400).json({ error: "name or number is missing" });
+    return res.status(400).json({ error: 'name or number is missing' })
   }
 
   if (await getPersonByName(newPerson.name)) {
-    return res.status(400).json({ error: "name must be unique" });
+    return res.status(400).json({ error: 'name must be unique' })
   }
 
   if (
-    typeof newPerson.name !== "string" ||
-    typeof newPerson.number !== "string"
+    typeof newPerson.name !== 'string' ||
+    typeof newPerson.number !== 'string'
   ) {
-    return res.status(400).json({ error: "name and number must be strings" });
+    return res.status(400).json({ error: 'name and number must be strings' })
   }
 
-  res.json(await addPerson(newPerson.name, newPerson.number));
-});
+  res.json(await addPerson(newPerson.name, newPerson.number))
+})
 
-app.put("/api/persons/:id", express.json(), async (req, res) => {
+app.put('/api/persons/:id', express.json(), async (req, res) => {
   const updatedPerson = {
     ...req.body,
-  };
-
-  if (!updatedPerson.name || !updatedPerson.number) {
-    return res.status(400).json({ error: "name or number is missing" });
   }
 
-  const person = await getPersonById(req.params.id);
+  if (!updatedPerson.name || !updatedPerson.number) {
+    return res.status(400).json({ error: 'name or number is missing' })
+  }
+
+  const person = await getPersonById(req.params.id)
   if (!person) {
-    return res.status(404).json({ error: "person not found" });
+    return res.status(404).json({ error: 'person not found' })
   }
 
   if (
-    typeof updatedPerson.name !== "string" ||
-    typeof updatedPerson.number !== "string"
+    typeof updatedPerson.name !== 'string' ||
+    typeof updatedPerson.number !== 'string'
   ) {
-    return res.status(400).json({ error: "name and number must be strings" });
+    return res.status(400).json({ error: 'name and number must be strings' })
   }
 
-  res.json(await updatePerson(req.params.id, updatedPerson));
-});
+  res.json(await updatePerson(req.params.id, updatedPerson))
+})
 
-const errorHandler = (err, req, res, next) => {
-  if (err.name === "CastError")
-    return res.status(400).json({ error: "invalid ID" });
+const errorHandler = (err, req, res) => {
+  if (err.name === 'CastError')
+    return res.status(400).json({ error: 'invalid ID' })
 
-  if (err.name == "ValidationError") {
-    return res.status(400).json({ error: err.message });
+  if (err.name === 'ValidationError') {
+    return res.status(400).json({ error: err.message })
   }
 
-  console.error(err);
-  res.status(500).json({ error: "internal server error" });
-};
-app.use(errorHandler);
+  console.error(err)
+  res.status(500).json({ error: 'internal server error' })
+}
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- part3/phonebook/src/mongo.js
+++ part3/phonebook/src/mongo.js
@@ -1,9 +1,9 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
+require('dotenv').config()
+const mongoose = require('mongoose')
 
 // this fixes querySrv problem with mongoose
 // https://www.mongodb.com/community/forums/t/error-querysrv-econnrefused-mongodb/259042
-require("node:dns/promises").setServers(["1.1.1.1"]);
+require('node:dns/promises').setServers(['1.1.1.1'])
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -19,75 +19,75 @@ const personSchema = new mongoose.Schema({
 
     validate: {
       validator: function (v) {
-        return /\d{2,3}-\d*/.test(v);
+        return /\d{2,3}-\d*/.test(v)
       },
       message: (props) =>
         `${props.value} is not a valid phone number! Number must be formed by two parts separated by a dash (-) and the first part must be 2 or 3 digits long.`,
     },
   },
-});
-const Person = mongoose.model("Person", personSchema);
+})
+const Person = mongoose.model('Person', personSchema)
 
 const getAllPersons = async () => {
-  return await Person.find({});
-};
+  return await Person.find({})
+}
 
 const getPersonById = async (id) => {
-  return await Person.find({ _id: id });
-};
+  return await Person.find({ _id: id })
+}
 
 const getPersonByName = async (name) => {
-  return await Person.find({ name }).then((persons) => persons[0]);
-};
+  return await Person.find({ name }).then((persons) => persons[0])
+}
 
 const addPerson = async (name, number) => {
   const person = new Person({
     name: name,
     number: number,
-  });
+  })
 
-  await person.save();
-  console.log(`added ${name} number ${number} to phonebook`);
-  return person;
-};
+  await person.save()
+  console.log(`added ${name} number ${number} to phonebook`)
+  return person
+}
 
 const deletePerson = async (id) => {
-  await Person.deleteOne({ _id: id });
-};
+  await Person.deleteOne({ _id: id })
+}
 
 const updatePerson = async (id, newObject) => {
   return await Person.findByIdAndUpdate(id, newObject, {
     new: true,
     runValidators: true,
-  });
-};
+  })
+}
 
 async function main() {
-  const name = process.argv[2];
-  const number = process.argv[3];
+  const name = process.argv[2]
+  const number = process.argv[3]
 
-  const mongodb_url = process.env.MONGODB_URL;
-  mongoose.set("strictQuery", false);
-  await mongoose.connect(mongodb_url, { family: 4 });
+  const mongodb_url = process.env.MONGODB_URL
+  mongoose.set('strictQuery', false)
+  await mongoose.connect(mongodb_url, { family: 4 })
 
   if (!name && !number) {
-    console.log("phonebook:");
-    const persons = await getAllPersons();
+    console.log('phonebook:')
+    const persons = await getAllPersons()
     for (const person of persons) {
-      console.log(`${person.name} ${person.number}`);
+      console.log(`${person.name} ${person.number}`)
     }
-    process.exit(0);
+    process.exit(0)
   } else if (!name || !number) {
-    console.log("Please provide both name and number as arguments");
-    process.exit(1);
+    console.log('Please provide both name and number as arguments')
+    process.exit(1)
   }
 
-  await addPerson(name, number);
-  process.exit(0);
+  await addPerson(name, number)
+  process.exit(0)
 }
 
 if (require.main === module) {
-  main();
+  main()
 }
 
 module.exports = {
@@ -97,4 +97,4 @@ module.exports = {
   getPersonByName,
   deletePerson,
   updatePerson,
-};
+}
