e02602c5fea54b9bb33784d682f25506502a8819
Joona Granlund <40266374+jonsetzky@users.noreply.github.com>
2026-01-22 18:50:58 +0200

    complete 5.18

--- part5/tests/tests/bloglist.spec.js
+++ part5/tests/tests/bloglist.spec.js
@@ -3,6 +3,15 @@ const { test, expect, describe, beforeEach } = require("@playwright/test");
 describe("Blog app", () => {
   beforeEach(async ({ page }) => {
     await page.goto("http://localhost:5173");
+
+    await page.request.post("http://localhost:5173/api/testing/reset");
+
+    await page.request.post("http://localhost:5173/api/users", {
+      data: {
+        username: "testuser",
+        password: "testpassword",
+      },
+    });
   });
   test("login form is shown", async ({ page }) => {
     await expect(
@@ -10,5 +19,25 @@ describe("Blog app", () => {
     ).toBeVisible();
     await expect(page.getByText("username", { exact: false })).toBeVisible();
     await expect(page.getByText("password", { exact: false })).toBeVisible();
+  });
+
+  describe("login", () => {
+    test("succeeds with correct credentials", async ({ page }) => {
+      await page.locator("#username").fill("testuser");
+      await page.locator("#password").fill("testpassword");
+      await page.getByText("login").click();
+
+      await expect(page.getByText("blogs")).toBeVisible();
+      await expect(page.getByText("testuser logged in")).toBeVisible();
+    });
+    test("fails with incorrect credentials", async ({ page }) => {
+      await page.locator("#username").fill("testuser");
+      await page.locator("#password").fill("wrongpassword");
+      await page.getByText("login").click();
+
+      await expect(
+        page.getByText("log in to application", { exact: false }),
+      ).toBeVisible();
+    });
   });
 });
