c44e78404036fc5f1f1a428004ba99cbbea57055
Joona Granlund <40266374+jonsetzky@users.noreply.github.com>
2026-01-22 16:06:30 +0200

    complete 5.7

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,52 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const [showDetails, setShowDetails] = useState(false);
+  return (
+    <div style={blogStyle}>
+      <i>{blog.title}</i> by {blog.author}
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? "collapse" : "expand"}
+      </button>
+      {showDetails && (
+        <table>
+          <tbody>
+            <tr>
+              <td>title</td>
+              <td>{blog.title}</td>
+            </tr>
+            <tr>
+              <td>author</td>
+              <td>{blog.author}</td>
+            </tr>
+            <tr>
+              <td>url</td>
+              <td>{blog.url}</td>
+            </tr>
+            <tr>
+              <td>likes</td>
+              <td>{blog.likes}</td>
+              <td>
+                <button>like</button>
+              </td>
+            </tr>
+            <tr>
+              <td>user</td>
+              <td>{blog.user?.name || blog.user?.username || "n/a"}</td>
+            </tr>
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
