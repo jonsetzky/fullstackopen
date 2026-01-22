f1eca6201c4bf0074cf94b975f5a516f5b4d3b4e
Joona Granlund <40266374+jonsetzky@users.noreply.github.com>
2026-01-22 13:08:07 +0200

    add login form material

--- part4/src/controllers/login.js
+++ part4/src/controllers/login.js
@@ -5,7 +5,28 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 loginController.post("/", async (request, response) => {
-  // todo
+  const { username, password } = request.body;
+
+  const user = await User.findOne({ username });
+  const passwordCorrect =
+    user === null ? false : await bcrypt.compare(password, user.password);
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: "invalid username or password",
+    });
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  };
+
+  const token = jwt.sign(userForToken, process.env.SECRET);
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name });
 });
 
 module.exports = loginController;
