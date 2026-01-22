3f4abd60a8baafc18040932568f7b9360c347d6e
Joona Granlund <40266374+jonsetzky@users.noreply.github.com>
2026-01-22 16:28:53 +0200

    complete 5.10

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -128,17 +128,19 @@ const App = () => {
         </button>
       </div>
       <div />
-      {blogs.map((blog) => (
-        <Blog
-          key={blog.id}
-          blog={blog}
-          updateBlog={(updatedBlog) => {
-            setBlogs(
-              blogs.map((b) => (b.id === updatedBlog.id ? updatedBlog : b)),
-            );
-          }}
-        />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog
+            key={blog.id}
+            blog={blog}
+            updateBlog={(updatedBlog) => {
+              setBlogs(
+                blogs.map((b) => (b.id === updatedBlog.id ? updatedBlog : b)),
+              );
+            }}
+          />
+        ))}
     </div>
   );
 };
