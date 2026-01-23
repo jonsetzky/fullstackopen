cb84b0cce5884bdb6b235abec543b4a6c3675aaf
Joona Granlund <40266374+jonsetzky@users.noreply.github.com>
2026-01-23 10:05:24 +0200

    modify filter reducer to use redux toolkit

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,19 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = "";
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "SET_FILTER":
-      return action.data.filter.slice(0);
-    default:
-      return state;
-  }
-};
+const filterSlice = createSlice({
+  name: "filter",
+  initialState,
+  reducers: {
+    setFilter(state, action) {
+      return action.payload;
+    },
+  },
+});
 
-export const setFilter = (filter) => {
-  return {
-    type: "SET_FILTER",
-    data: { filter },
-  };
-};
-
-export default reducer;
+export const { setFilter } = filterSlice.actions;
+export default filterSlice.reducer;
