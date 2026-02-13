95be72acd8b1882d69119bc766b052387f34b147
jonsetzky <40266374+jonsetzky@users.noreply.github.com>
2026-02-13 20:39:28 +0200

    complete 9.9

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.get("/ping", (_req, res) => {
+app.get("/api/ping", (_req, res) => {
   res.send("pong");
 });
 
-app.listen(3003, () => {
-  console.log("Server is running on http://localhost:3003");
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
