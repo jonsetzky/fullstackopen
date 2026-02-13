9ed6c0742b3da77736450217d888c949d0023798
jonsetzky <40266374+jonsetzky@users.noreply.github.com>
2026-02-14 00:20:58 +0200

    complete 9.19

--- part9/flight-diary-frontend/src/components/DiaryForm.tsx
+++ part9/flight-diary-frontend/src/components/DiaryForm.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useField } from "../hooks";
 import diaryService from "../services/diary";
 import type { DiaryData } from "../types";
+import axios from "axios";
 
 export const DiaryForm = () => {
   const { reset: resetDate, ...date } = useField("text");
   const { reset: resetVisibility, ...visibility } = useField("text");
   const { reset: resetWeather, ...weather } = useField("text");
   const { reset: resetComment, ...comment } = useField("text");
+  const [error, setError] = useState("");
 
   const reset = () => {
     resetDate();
@@ -16,18 +18,39 @@ export const DiaryForm = () => {
     resetComment();
   };
 
-  const submit: React.SubmitEventHandler<HTMLFormElement> = () => {
-    diaryService.create({
-      comment: comment.value,
-      date: date.value,
-      visibility: visibility.value as DiaryData["visibility"], // todo validate
-      weather: weather.value as DiaryData["weather"], // todo validate
-    });
-    reset();
+  const submit: React.SubmitEventHandler<HTMLFormElement> = async (event) => {
+    event.preventDefault();
+    try {
+      await diaryService.create({
+        comment: comment.value,
+        date: date.value,
+        visibility: visibility.value as DiaryData["visibility"], // todo validate
+        weather: weather.value as DiaryData["weather"], // todo validate
+      });
+      reset();
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        if (
+          error.status === 400 &&
+          error.response &&
+          "data" in error.response &&
+          typeof error.response.data === "string"
+        ) {
+          setError(error.response.data.replace("Something went wrong. ", ""));
+          return;
+        }
+        console.error("unknown axios error", error.toJSON());
+        return;
+      }
+      console.error("unknown error", error);
+    }
+    return false;
   };
 
   return (
     <form onSubmit={submit}>
+      <h2>Add a new entry</h2>
+      <p style={{ color: "#f00" }}>{error}</p>
       <div>
         date <input {...date} />
       </div>
