472297d38b7abc820d576b605b13d70cd97c4e31
jonsetzky <40266374+jonsetzky@users.noreply.github.com>
2026-02-14 01:19:48 +0200

    complete 9.22

--- part9/patientor-frontend/src/components/PatientPage/index.tsx
+++ part9/patientor-frontend/src/components/PatientPage/index.tsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+import { Patient } from "../../types";
+import patientService from "../../services/patients";
+import { useParams } from "react-router-dom";
+
 const PatientPage = () => {
-  return <div>PatientPage</div>;
+  const params = useParams();
+  const [patient, setPatient] = useState<Patient>();
+
+  useEffect(() => {
+    (async () => {
+      if (params.id) {
+        setPatient(await patientService.get(params.id));
+      }
+    })();
+  }, []);
+
+  if (!patient) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>{patient.name}</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>gender</td>
+            <td>{patient.gender}</td>
+          </tr>
+          <tr>
+            <td>ssn</td>
+            <td>{patient.ssn}</td>
+          </tr>
+          <tr>
+            <td>occupation</td>
+            <td>{patient.occupation}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default PatientPage;
