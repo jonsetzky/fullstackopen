73bd7161b45e7ab007fee379a4cbc7ea98cc7b28
jonsetzky <40266374+jonsetzky@users.noreply.github.com>
2026-02-14 02:03:11 +0200

    add validation fro 9.27

--- part9/patientor-backend/src/validation/patient.ts
+++ part9/patientor-backend/src/validation/patient.ts
@@ -1,9 +1,40 @@
 import z from "zod";
 import { Gender } from "../types";
 
+const dateSchema = () => z.string().regex(/\d{4}\-\d{2}-\d{2}/);
+
+const baseEntrySchema = () => ({
+  date: dateSchema(),
+  specialist: z.string(),
+  diagnosisCodes: z.array(z.string()),
+  description: z.string(),
+});
+
+const newHospitalEntrySchema = z.object({
+  ...baseEntrySchema(),
+  type: "Hospital",
+  discharge: z.object({
+    date: dateSchema(),
+    criteria: z.string(),
+  }),
+});
+export type NewHospitalEntry = z.infer<typeof newHospitalEntrySchema>;
+
+const newOccupationalHealthcareEntrySchema = z.object({
+  ...baseEntrySchema(),
+  type: "OccupationalHealthcare",
+  sickLeave: z.object({
+    startDate: dateSchema(),
+    endDate: dateSchema(),
+  }),
+});
+export type NewOccupationalHealthCareEntry = z.infer<
+  typeof newOccupationalHealthcareEntrySchema
+>;
+
 const newPatientSchema = z.object({
   name: z.string(),
-  dateOfBirth: z.string().regex(/\d{4}\-\d{2}-\d{2}/),
+  dateOfBirth: dateSchema(),
   ssn: z.string().regex(/\d{6}-[A-Z0-9]{3,4}/),
   gender: z.enum(Gender),
   occupation: z.string(),
@@ -13,3 +44,11 @@ export type NewPatient = z.infer<typeof newPatientSchema>;
 
 export const toNewPatient = (object: unknown): NewPatient =>
   newPatientSchema.parse(object);
+
+export const toNewHospitalEntry = (object: unknown): NewHospitalEntry =>
+  newHospitalEntrySchema.parse(object);
+
+export const toNewOccupationalHealthcareEntry = (
+  object: unknown,
+): NewOccupationalHealthCareEntry =>
+  newOccupationalHealthcareEntrySchema.parse(object);
